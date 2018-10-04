cc089f6bfa8ce61bacb2e492d77f3ffb25f46743
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 22:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ function currentLine(line) {
   if (line.length>0) {
     var out="The line is currently: "
     for (let i=0;i<line.length;i++) {
-      out+=`${i+1}. ${line[i]},`
+      if (i===line.length-1) {
+        out+=`${i+1}. ${line[i]}`
+      }
+      else {
+        out+=`${i+1}. ${line[i]},`
+      }
     }
     return out;
   }
