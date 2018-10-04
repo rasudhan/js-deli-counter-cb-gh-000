f700b699f7026e7b2b0f483515efed9c38a81881
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 22:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(currentLine,name) {
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(currentLine) {
+  if (currentLine.length>0) {
+    return currentLine.shift()
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
